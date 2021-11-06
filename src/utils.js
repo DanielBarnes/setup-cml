46b7f7569e00282547f49f7bd0e61809369ddaed
Daniel Barnes <dabarnes2b@gmail.com>
2021-11-05 21:21:15 -0700

    Debug options (#1)
    * log version for validation
    
    * use npm to grab ver
    
    * npm list and parse
    
    * handle errors
    
    * typo
    
    * review changes
    
    * pass with no sudo
    * fail when unable to parse version
    
    * override option for git install
    
    * clarify installed version log line

--- src/utils.js
+++ src/utils.js
@@ -16,7 +16,8 @@ const exec = async (command, opts) => {
 };
 
 const setupCml = async opts => {
-  const { version, sudo = true } = opts;
+  const { version, sudo = true, override = '' } = opts;
+  const orFlag = override !== '';
   let sudoPath = '';
   if (sudo) {
     try {
@@ -25,19 +26,36 @@ const setupCml = async opts => {
   }
 
   await exec(`${sudoPath} npm config set user 0`);
-
   console.log('Uninstalling previous CML');
   await exec(
-    `${sudoPath} npm uninstall -g canvas vega vega-cli vega-lite @dvcorg/cml`
-  );
-
-  console.log(`Installing CML version ${version}`);
-  await exec('npm config set user 0');
-  await exec(
-    `${sudoPath} npm install -g canvas@2 vega@5 vega-cli@5 vega-lite@4 @dvcorg/cml${
-      version !== 'latest' ? `@${version}` : ''
+    `${sudoPath} npm uninstall -g canvas vega vega-cli vega-lite @dvcorg/cml ${
+      orFlag ? override : ''
     }`
   );
+
+  console.log(`Installing CML version ${orFlag ? override : version}`);
+  await exec('npm config set user 0');
+  await exec(
+    `${sudoPath} npm install -g canvas@2 vega@5 vega-cli@5 vega-lite@4 ${
+      orFlag
+        ? override
+        : `@dvcorg/cml${version !== 'latest' ? `@${version}` : ''}`
+    }`
+  );
+
+  let installedVersion = '';
+  try {
+    const json = await exec('npm list --json --global @dvcorg/cml');
+    installedVersion = JSON.parse(json).dependencies['@dvcorg/cml'].version;
+    console.log(
+      `Targeted CML version: ${version}, received: ${installedVersion} ${
+        orFlag ? `but from overide at: ${override}` : ''
+      }`
+    );
+  } catch (err) {
+    console.log('Failed to parse the install version of CML.', err);
+    process.exit(1);
+  }
 };
 
 exports.exec = exec;
